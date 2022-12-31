08b67eebff76136bd254a43161d636b598533694
valenn0101 <veroncaceresvalentin@gmail.com>
2022-12-31 15:48:43 -0300

    Update tarea-clase-7.js

--- tarea/tarea-clase-7.js
+++ tarea/tarea-clase-7.js
@@ -1,6 +1,8 @@
 /*
 * Hacer las funciones de validación de validarCiudad y validarDescripcionRegalo.
 * Escribir pruebas para esas funciones.
+usar RegExp, forEach, Objetos, pruebas, estilos en los errores
+ y mostrar los errores en una lista
 *
 * Adicional: Escribir pruebas para las funciones de tareas anteriores.
 *
